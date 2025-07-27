b46e8c77c891f93387a0b9c13afc788669a08d53
Ryan Flores <rryanflorres@gmail.com>
2025-07-26 21:59:47 -0700

    Add Player and PlayerTests

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player {
+    public class Player : MonoBehaviour {
+        public int Health { get; set; } = 100;
+
+        public void TakeDamage(int damage) {
+            Health -= damage;
+            if (Health <= 0) Destroy(gameObject);
+        }
+    }
+}
